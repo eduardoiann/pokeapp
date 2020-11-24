4f041c682f1ad07d8530590ebc35c27d58836c15
Eduardo Caper Iannicelli Moreira <edw.iannicelli@gmail.com>
2020-11-24 11:25:25 -0300

    Deploy test

--- src/components/DetailsCard/Details.jsx
+++ src/components/DetailsCard/Details.jsx
@@ -5,7 +5,7 @@ import './pokeDetailsCard.scss';
 
 export default function Details({ props: { card } }) {
 
-  /* Esse ternário verifica se a propriedade 'attacks' existe dentro de card. Caso não exista, isso significa que a carta não é um pokemon, e sim um item. Portanto, faz a chamada do componente <ItemDetailsCard /> passando como prop o card. Do contrário, significa que existe 'attacks', e ai renderiza o componente <PokeDetailsCard />*/
+  /* Esse ternário verifica se a propriedade 'attacks' existe dentro de card. Caso não exista, isso significa que a carta não é um pokemon, e sim um item. Portanto, faz a chamada do componente <ItemDetailsCard card={card}/> passando como prop o card. Do contrário, significa que existe 'attacks', e ai renderiza o componente <PokeDetailsCard />*/
 
   return card.attacks === undefined ? (
     <ItemDetailsCard card={card} />
